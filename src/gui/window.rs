b036482b6f6af4386cdac76a88400e7437feef80
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-03-07 16:39:02 +0530

    gui: window

--- src/gui/window.rs
+++ src/gui/window.rs
@@ -4,7 +4,9 @@
 */
 
 use minifb::{Key, Window, WindowOptions};
+
 use crate::engine::layout::LayoutBox;
+use crate::engine::paint;
 
 pub struct WindowApp {
     width: usize,
@@ -13,7 +15,9 @@ pub struct WindowApp {
 }
 
 impl WindowApp {
+
     pub fn new(width: usize, height: usize) -> Self {
+
         Self {
             width,
             height,
@@ -22,39 +26,32 @@ impl WindowApp {
     }
 
     pub fn run(&mut self, layout: Vec<LayoutBox>) {
+
         let mut window = Window::new(
             "Vortexa",
             self.width,
             self.height,
             WindowOptions::default(),
-        )
-            .unwrap();
+        ).unwrap();
 
         while window.is_open() && !window.is_key_down(Key::Escape) {
+
             self.clear();
 
-            for box_item in &layout {
-                self.draw_rect(box_item.x, box_item.y, box_item.width, box_item.height);
-            }
+            paint::paint(&layout, &mut self.buffer, self.width);
 
-            window
-                .update_with_buffer(&self.buffer, self.width, self.height)
-                .unwrap();
+            window.update_with_buffer(
+                &self.buffer,
+                self.width,
+                self.height
+            ).unwrap();
         }
     }
 
     fn clear(&mut self) {
+
         for pixel in self.buffer.iter_mut() {
             *pixel = 0xffffff;
-        }
-    }
-
-    fn draw_rect(&mut self, x: usize, y: usize, w: usize, h: usize) {
-        for yy in y..(y + h).min(self.height) {
-            for xx in x..(x + w).min(self.width) {
-                let idx = yy * self.width + xx;
-                self.buffer[idx] = 0x000000;
-            }
         }
     }
 }
